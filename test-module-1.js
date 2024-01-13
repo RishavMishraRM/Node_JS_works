44f1c9edd99e7eccdf7f53667167a0a0604e1438
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2024-01-14 01:16:47 +0530

    update

--- test-module-1.js
+++ test-module-1.js
@@ -2,25 +2,22 @@
 //   add(a, b) {
 //     return a + b;
 //   }
-
 //   multiply(a, b) {
 //     return a * b;
 //   }
-
 //   divide(a, b) {
 //     return a / b;
 //   }
 // }
 
+
 module.exports = class {
   add(a, b) {
     return a + b;
   }
-
   multiply(a, b) {
     return a * b;
   }
-
   divide(a, b) {
     return a / b;
   }
